ebdd1a0a4ccb418944e4e47402393f1db2bf3577
vramaniuk <astyanax@mail.ru>
2017-05-30 16:41:34 +0300

    configuration 11

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/src'
+        publicPath: '/'
     },
     watch: !isProduction,
     devtool: !isProduction && 'cheap-inline-module-source-map',
