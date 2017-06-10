034f40e631aaab348819b9e2aebb1eb1291f2d29
vramaniuk <astyanax@mail.ru>
2017-06-10 11:12:21 +0300

    conf5 without bandle

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-// import promise from 'redux-promise';
 import {createLogger} from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
 import rootReducer from '../reducers';
 
 const middlewares = [ thunk ];
