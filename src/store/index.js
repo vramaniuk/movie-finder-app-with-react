3bc01c94e1ab66810d644dd7f5af84d4eaacb388
vramaniuk <astyanax@mail.ru>
2017-06-29 13:05:56 +0300

    without devtools extentions

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import rootReducer from '../reducers';
 
+const composeEnhancers = composeWithDevTools({
+  // options like actionSanitizer, stateSanitizer
+});
 const middlewares = [thunk];
 if (process.env.NODE_ENV !== 'production') {
-  middlewares.push(createLogger());
+  middlewares.push(logger);
 }
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
+const store = createStore(rootReducer,
+  /* preloadedState, */
+  composeEnhancers(
+  applyMiddleware(...middlewares),
+));
 
 export default store;
